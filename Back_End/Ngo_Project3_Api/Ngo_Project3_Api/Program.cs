8142b91557b7cbfba7466aa4dc25e2940124d05e
tranxuanbach <tranbach2711@gmail.com>
2025-01-06 07:59:46 +0700

    middle ware cmt

--- Back_End/Ngo_Project3_Api/Ngo_Project3_Api/Program.cs
+++ Back_End/Ngo_Project3_Api/Ngo_Project3_Api/Program.cs
@@ -1,4 +1,8 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+using Ngo_Project3_Api.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
 
 
 // Thêm CORS service
@@ -14,15 +18,49 @@ builder.Services.AddCors(options =>
 });
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/*builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+    {
+        Description = "API Key cần nhập vào header. Ví dụ: X-API-Key: {API_KEY}",
+        In = ParameterLocation.Header,
+        Name = "X-API-Key",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "ApiKeyScheme"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                },
+                Scheme = "ApiKeyScheme",
+                Name = "ApiKey",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});*/
 
 var app = builder.Build();
 
 app.UseCors("AllowAllOrigins");
+
+//app.UseCustomMiddleware();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
